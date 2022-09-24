43359d675ac602ca4104456ce7b2b558107a8101
Alan <alan.andersonbr@gmail.com>
2022-09-23 21:37:31 -0300

    feat: craeting value object

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -2,23 +2,15 @@ package br.com.erudio.data.vo.v1
 
 import jakarta.persistence.*
 
-@Entity
-@Table(name= "person")
 
-data class Person (
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+data class PersonVO (
+
+
 var id: Long = 0,
-//nullabe for cant set null value in the column
-    @Column(name = "first_name", nullable = false, length = 40)
 var firstName: String = "",
-    @Column(name = "last_name", length = 80)
 var lastName: String = "",
-
-@Column(nullable = false, length = 60)
 var address: String = "",
-    @Column(nullable = false, length = 8)
 var gender: String = "",
 
 
