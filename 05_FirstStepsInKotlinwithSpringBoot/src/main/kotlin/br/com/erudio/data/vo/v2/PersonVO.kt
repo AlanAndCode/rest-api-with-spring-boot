0a6a99da4a8beb8494cbdb59cdc109e4490da2ac
Alan <alan.andersonbr@gmail.com>
2022-09-24 21:20:47 -0300

    fix: fixing dozer version so you can version the project

---
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v2/PersonVO.kt
@@ -0,0 +1,17 @@
+package br.com.erudio.data.vo.v2
+
+import jakarta.persistence.*
+import java.util.Date
+
+
+data class PersonVO2 (
+
+
+var id: Long = 0,
+var firstName: String = "",
+var lastName: String = "",
+var address: String = "",
+var gender: String = "",
+var birthDay: Date? = null
+
+)
