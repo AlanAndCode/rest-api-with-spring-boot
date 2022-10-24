20014f37d2b7333a239e3671239b5efb18b58d65
Alan <alan.andersonbr@gmail.com>
2022-10-24 00:39:22 -0300

    feat: changing class element name

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,17 +1,23 @@
 package br.com.erudio.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import jakarta.persistence.*
 
 
-
+@JsonPropertyOrder("id", "address", "first_name", "last_name", "gender")
 data class PersonVO (
 
 
-var id: Long = 0,
-var firstName: String = "",
-var lastName: String = "",
-var address: String = "",
-var gender: String = "",
+    var id: Long = 0,
+    @field:JsonProperty("first_name")
+    var firstName: String = "",
 
+    @field:JsonProperty("last_name")
+    var lastName: String = "",
+    var address: String = "",
 
+    @field:JsonIgnore
+    var gender: String = "",
 )
