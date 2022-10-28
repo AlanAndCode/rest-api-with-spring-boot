9256244207ec910843e0726eca07c0e32fb87b50
Alan <alan.andersonbr@gmail.com>
2022-10-28 01:38:36 -0300

    refactor: add yaml

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/config/WebConfig.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/config/WebConfig.kt
@@ -1,14 +1,24 @@
 package br.com.erudio.config
 
+import br.com.erudio.serialzation.converter.YamlJackson2HttpMessageConverter
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig: WebMvcConfigurer {
 
+
+
+        private val MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("applicaion/yml")
+
+        override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
+            converters.add(YamlJackson2HttpMessageConverter())
+        }
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+        
    /*
    queryParameter
    configurer.favorParameter(true)
@@ -25,5 +35,6 @@ class WebConfig: WebMvcConfigurer {
             .defaultContentType(MediaType.APPLICATION_JSON)
             .mediaType("json", MediaType.APPLICATION_JSON)
             .mediaType("xml", MediaType.APPLICATION_XML)
+            .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
     }
 }
