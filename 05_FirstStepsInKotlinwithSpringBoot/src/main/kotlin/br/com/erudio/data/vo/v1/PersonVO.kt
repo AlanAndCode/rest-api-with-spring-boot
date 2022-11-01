fd7afc53c7a745db8d876dd39dda38abb9d00a13
Alan <alan.andersonbr@gmail.com>
2022-10-31 23:09:03 -0300

    feat:implementing hateoas in person

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -3,15 +3,17 @@ package br.com.erudio.data.vo.v1
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.github.dozermapper.core.Mapping
 import jakarta.persistence.*
+import org.springframework.hateoas.RepresentationModel
 
 
 data class PersonVO (
 
-
-    var id: Long = 0,
+    @Mapping("id")
+    var key: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
     var address: String = "",
     var gender: String = "",
-)
+) : RepresentationModel<PersonVO>()
