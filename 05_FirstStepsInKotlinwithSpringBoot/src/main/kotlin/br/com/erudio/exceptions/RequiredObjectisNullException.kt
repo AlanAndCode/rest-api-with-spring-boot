b8eaf888822bc929fc59e6db34eefc7bf055262b
Alan <alan.andersonbr@gmail.com>
2022-11-05 01:32:09 -0300

    feat: creating Exception for create and update

---
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/exceptions/RequiredObjectisNullException.kt
@@ -0,0 +1,12 @@
+package br.com.erudio.exceptions
+
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.lang.*
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class RequiredObjectisNullException: RuntimeException {
+    constructor(): super("It is not allowed to persist a null object!")
+    constructor(exception: String?): super(exception)
+
+}
