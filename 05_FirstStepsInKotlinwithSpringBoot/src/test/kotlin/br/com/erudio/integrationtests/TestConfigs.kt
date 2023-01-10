3865d7efd694f7a63cacfb01dba216b3e24f5565
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-01-09 23:08:24 -0300

    test: update a confgs test

--- 05_FirstStepsInKotlinwithSpringBoot/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
@@ -2,7 +2,6 @@ package br.com.erudio.integrationtests
 
 object TestConfigs {
     const val SERVER_PORT = 8888
-
     const val HEADER_PARAM_ORIGIN = "Origin"
 
     const val CONTENT_TYPE_JSON = "application/json"
