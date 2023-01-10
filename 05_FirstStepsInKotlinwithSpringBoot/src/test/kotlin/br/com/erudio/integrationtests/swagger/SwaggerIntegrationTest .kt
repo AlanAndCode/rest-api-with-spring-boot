3b695b166247d11448d9ea5d68bf3822326bccc0
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-01-09 23:06:33 -0300

    fix: create a swagger test

--- 05_FirstStepsInKotlinwithSpringBoot/src/test/kotlin/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest .kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/test/kotlin/br/com/erudio/integrationtests/swagger/SwaggerIntegrationTest .kt
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SwaggerIntegrationTest() : AbstractIntegrationTest() {
-
 	@Test
 	fun shouldDisplaySwaggerUiPage() {
 		val content = given()
