c004a84e69e8eaaa5357df6851ed5e9c49833a42
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-01-11 17:20:35 -0300

    feat: implment a var enabled Boolean

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/model/Person.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/model/Person.kt
@@ -3,23 +3,25 @@ package br.com.erudio.model
 import jakarta.persistence.*
 
 @Entity
-@Table(name= "person")
-
+@Table(name = "person")
 data class Person (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-var id: Long = 0,
-//nullabe for cant set null value in the column
-    @Column(name = "first_name", nullable = false, length = 40)
-var firstName: String = "",
-    @Column(name = "last_name", length = 80)
-var lastName: String = "",
+    var id: Long = 0,
 
-@Column(nullable = false, length = 60)
-var address: String = "",
-    @Column(nullable = false, length = 8)
-var gender: String = "",
+    @Column(name = "first_name", nullable = false, length = 80)
+    var firstName: String = "",
 
+    @Column(name = "last_name", nullable = false, length = 80)
+    var lastName: String = "",
 
+    @Column(nullable = false, length = 100)
+    var address: String = "",
+
+    @Column(nullable = false, length = 6)
+    var gender: String = "",
+
+    @Column(nullable = false)
+    var enabled: Boolean = true
 )
