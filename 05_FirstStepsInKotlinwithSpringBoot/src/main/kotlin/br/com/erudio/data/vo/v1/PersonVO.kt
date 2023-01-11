a599aa58728afaf09d217070c04e121ac8e89a21
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-01-11 16:57:44 -0300

    feat: add enabled in boolean

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,13 +1,12 @@
 package br.com.erudio.data.vo.v1
 
-import com.fasterxml.jackson.annotation.JsonIgnore
+
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.github.dozermapper.core.Mapping
-import jakarta.persistence.*
 import org.springframework.hateoas.RepresentationModel
 
-@JsonPropertyOrder("id", "firstName", "lastName", "address", "gender")
+@JsonPropertyOrder("id", "firstName", "lastName", "address", "gender", "enabled")
 data class PersonVO (
 
     @Mapping("id")
@@ -17,4 +16,5 @@ data class PersonVO (
     var lastName: String = "",
     var address: String = "",
     var gender: String = "",
+    var enabled: Boolean = true
 ) : RepresentationModel<PersonVO>()
