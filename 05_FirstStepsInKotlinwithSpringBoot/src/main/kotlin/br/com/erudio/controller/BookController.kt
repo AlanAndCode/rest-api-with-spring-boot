a06aa1df8a9452973570dd89ba3138ac5c11d0ab
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-01-20 23:59:12 -0300

    feat: implement pages in controller

--- 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/controller/BookController.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/main/kotlin/br/com/erudio/controller/BookController.kt
@@ -1,7 +1,5 @@
 package br.com.erudio.controller
 
-
-
 import br.com.erudio.data.vo.v1.BookVO
 import br.com.erudio.services.BookService
 import br.com.erudio.util.MediaType
@@ -12,34 +10,36 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
-
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.PagedModel
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-
 @RestController
-@RequestMapping("/api/Books/v1")
+@RequestMapping("/api/book/v1")
 @Tag(name = "Books", description = "Endpoints for Managing Books")
 class BookController {
-
 
     @Autowired
     private lateinit var service: BookService
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
-    @Operation(summary = "find all Bookss", description = "finds all Books",
-    tags = ["Books"],
+    @Operation(summary = "Finds all Books", description = "Finds all Books",
+        tags = ["Books"],
         responses = [
             ApiResponse(
-                description = "Sucess",
+                description = "Success",
                 responseCode = "200",
                 content = [
                     Content(array = ArraySchema(schema = Schema(implementation = BookVO::class)))
                 ]
             ),
-        ApiResponse(description = "No Content", responseCode = "204", content = [
-            Content(schema = Schema(implementation = Unit::class))
-        ]),
+            ApiResponse(description = "No Content", responseCode = "204", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
             ApiResponse(description = "Bad Request", responseCode = "400", content = [
                 Content(schema = Schema(implementation = Unit::class))
             ]),
@@ -53,19 +53,23 @@ class BookController {
                 Content(schema = Schema(implementation = Unit::class))
             ]),
         ]
-        )
-    fun findAll():List<BookVO> {
-
-// if call Get without id, fall in method findAll
-        return service.findAll()
-
+    )
+    fun findAll(@RequestParam(value = "page", defaultValue = "0") page: Int,
+                @RequestParam(value = "size", defaultValue = "12") size: Int,
+                @RequestParam(value = "direction", defaultValue = "asc") direction: String
+    ): ResponseEntity<PagedModel<EntityModel<BookVO>>> {
+        val sortDirection: Sort.Direction = if ("desc".equals(direction, ignoreCase = true)) Sort.Direction.DESC else Sort.Direction.ASC
+        val pageable: Pageable = PageRequest.of(page, size , Sort.by(sortDirection, "title"))
+        return ResponseEntity.ok(service.findAll(pageable))
     }
-    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
-    @Operation(summary = "finds a Books", description = "finds a Books",
+
+    @GetMapping(value = ["/{id}"],
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
+    @Operation(summary = "Finds a Book", description = "Finds a Book",
         tags = ["Books"],
         responses = [
             ApiResponse(
-                description = "Sucess",
+                description = "Success",
                 responseCode = "200",
                 content = [
                     Content(schema = Schema(implementation = BookVO::class))
@@ -88,18 +92,17 @@ class BookController {
             ]),
         ]
     )
-    fun findById(@PathVariable(value = "id")id: Long,
-    ): BookVO {
-
+    fun findById(@PathVariable(value="id") id: Long): BookVO {
         return service.findById(id)
     }
+
     @PostMapping(consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
-    produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
-    @Operation(summary = "Adds a Books", description = "Adds  a Books",
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
+    @Operation(summary = "Adds a new Book", description = "Adds a new Book",
         tags = ["Books"],
         responses = [
             ApiResponse(
-                description = "Sucess",
+                description = "Success",
                 responseCode = "200",
                 content = [
                     Content(schema = Schema(implementation = BookVO::class))
@@ -116,18 +119,18 @@ class BookController {
             ]),
         ]
     )
-    fun create(@RequestBody Books: BookVO): BookVO {
+    fun create(@RequestBody book: BookVO): BookVO {
+        return service.create(book)
 
-        return service.create(Books)
     }
 
     @PutMapping(consumes = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
-    produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
-    @Operation(summary = "update a Books information", description = "update a Books information",
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
+    @Operation(summary = "Updates a book's information", description = "Updates a book's information",
         tags = ["Books"],
         responses = [
             ApiResponse(
-                description = "Sucess",
+                description = "Success",
                 responseCode = "200",
                 content = [
                     Content(schema = Schema(implementation = BookVO::class))
@@ -150,15 +153,15 @@ class BookController {
             ]),
         ]
     )
-        fun update(@RequestBody Books: BookVO): BookVO {
-
-        return service.update(Books)
+    fun update(@RequestBody book: BookVO): BookVO {
+        return service.update(book)
     }
-    @DeleteMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
-    @Operation(summary = "delete a Books", description = "delete a Books",
+
+    @DeleteMapping(value = ["/{id}"],
+        produces = [MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
+    @Operation(summary = "Deletes a book", description = "Deletes a book",
         tags = ["Books"],
         responses = [
-
             ApiResponse(description = "No Content", responseCode = "204", content = [
                 Content(schema = Schema(implementation = Unit::class))
             ]),
@@ -176,11 +179,8 @@ class BookController {
             ]),
         ]
     )
-    fun delete(@PathVariable(value = "id")id: Long) : ResponseEntity<*>{
-       service.delete(id)
+    fun delete(@PathVariable(value="id") id: Long) : ResponseEntity<*>{
+        service.delete(id)
         return ResponseEntity.noContent().build<Any>()
     }
-
-
 }
-
