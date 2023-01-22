645abbda923635d75e0abc33a7dbf62ef2ebe74a
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-01-22 13:00:20 -0300

    refactor: change Server_Port in configs

--- 05_FirstStepsInKotlinwithSpringBoot/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
+++ 05_FirstStepsInKotlinwithSpringBoot/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
@@ -1,14 +1,15 @@
 package br.com.erudio.integrationtests
 
 object TestConfigs {
-    const val SERVER_PORT = 8888
+    const val SERVER_PORT = 8080
+
     const val HEADER_PARAM_ORIGIN = "Origin"
     const val HEADER_PARAM_AUTHORIZATION = "Authorization"
 
-
     const val CONTENT_TYPE_JSON = "application/json"
     const val CONTENT_TYPE_XML = "application/xml"
-    const val CONTENT_TYPE_YAML = "application/x-yaml"
+
+    const val CONTENT_TYPE_YML = "application/x-yaml"
 
     const val ORIGIN_ERUDIO = "https://erudio.com.br"
     const val ORIGIN_SEMERU = "https://semeru.com.br"
