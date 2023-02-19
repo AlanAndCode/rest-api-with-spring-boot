aeac1c8d74f287b21d0586c5d8b4b87a64e8f249
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-02-19 12:40:25 -0300

    feat: postgres

--- src/main/kotlin/br/com/api/data/vo/v1/BookVO.kt
+++ src/main/kotlin/br/com/api/data/vo/v1/BookVO.kt
@@ -13,10 +13,12 @@ data class BookVO (
 
     @Mapping("id")
     @field:JsonProperty("id")
-var key: Long = 0,
-var author: String = "",
-var launchDate: Date? = null,
-var price: Double = 0.0,
-var title: String = ""
+    var key: Long = 0,
+    var author: String = "",
+    var launchDate: Date? = null,
+    var price: Double = 0.0,
+    var title: String = ""
 
-    ): RepresentationModel<BookVO>()
+): RepresentationModel<BookVO>() {
+
+}
--- src/test/kotlin/br/com/api/integrationtests/testconainers/AbstractIntegrationTest .kt
+++ src/test/kotlin/br/com/api/integrationtests/testconainers/AbstractIntegrationTest .kt
@@ -4,13 +4,14 @@ import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.core.env.MapPropertySource
 import org.springframework.test.context.ContextConfiguration
-import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.lifecycle.Startables
 import java.util.stream.Stream
 
 @ContextConfiguration(initializers = [AbstractIntegrationTest.Initializer::class])
 open class AbstractIntegrationTest {
-    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext>{
+
+    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
 
         override fun initialize(applicationContext: ConfigurableApplicationContext) {
             startContainers()
@@ -24,17 +25,17 @@ open class AbstractIntegrationTest {
 
         companion object {
 
-            private var mysql: MySQLContainer<*> = MySQLContainer("mysql:8.0.28")
+            private var postgres: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14.2")
 
             private fun startContainers() {
-                Startables.deepStart(Stream.of(mysql)).join()
+                Startables.deepStart(Stream.of(postgres)).join()
             }
 
             private fun createConnectionConfiguration(): MutableMap<String, Any> {
                 return java.util.Map.of(
-                    "spring.datasource.url", mysql.jdbcUrl,
-                    "spring.datasource.username", mysql.username,
-                    "spring.datasource.password", mysql.password,
+                    "spring.datasource.url", postgres.jdbcUrl,
+                    "spring.datasource.username", postgres.username,
+                    "spring.datasource.password", postgres.password,
                 )
             }
         }
